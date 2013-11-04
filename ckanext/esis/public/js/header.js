eeb3872f128060968508bea34e6ce705f2e3802e
Justin Merz <jrmerz@gmail.com>
2013-11-04 13:45:36 -0800

    fixes for js resources

--- ckanext/esis/public/js/header.js
+++ ckanext/esis/public/js/header.js
@@ -2,8 +2,8 @@
 var path = window.location.pathname.split("/");
 if( path.length > 1 ) {
     if( path[1] == "dataset" ) {
-        $("header.masthead nav li a[href=/spectral]").parent().removeClass("active");
+        $("header.masthead nav li a[href='/spectral']").parent().removeClass("active");
     } else if ( path[1] == "spectral" ) {
-        $("header.masthead nav li a[href=/dataset]").parent().removeClass("active");
+        $("header.masthead nav li a[href='/dataset']").parent().removeClass("active");
     }
 }
