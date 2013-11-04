a53cbe4aa7979bb1ce1c64b29ce660a5952a82fc
Justin Merz <jrmerz@gmail.com>
2013-11-04 13:49:31 -0800

    remove unneeded controller

--- ckanext/esis/plugin.py
+++ ckanext/esis/plugin.py
@@ -84,7 +84,6 @@ class EsisPlugin(plugins.SingletonPlugin,
         return 'spectral/new_package_form.html'
 
     def before_map(self, map):
-        controller = 'ckanext.esis.view:ViewController'
         # Most of the routes are defined via the IDatasetForm interface
         # (ie they are the ones for a package type)
 
