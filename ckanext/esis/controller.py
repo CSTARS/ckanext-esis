0409ad52010e0c9ab55b40126f1ec29b34923623
Justin Merz <jrmerz@gmail.com>
2014-08-08 12:18:23 -0400

    adding python connection to mongodb for spectra storage

--- ckanext/esis/controller.py
+++ ckanext/esis/controller.py
@@ -2,6 +2,8 @@ import logging
 import re, zlib, StringIO
 #import simplejson as json
 import ujson as json
+from pymongo import MongoClient
+from pylons import config
 
 import ckan.lib.munge as munge
 
@@ -16,192 +18,100 @@ from multiprocessing import Process, Queue
 
 log = logging.getLogger(__name__)
 
+# setup mongo connection
+client = MongoClient(config._process_configs[1]['esis.mongo.url'])
+db = client[config._process_configs[1]['esis.mongo.db']]
+dataCollection = db[config._process_configs[1]['esis.mongo.data_collection']]
+infoCollection = db[config._process_configs[1]['esis.mongo.info_collection']]
+
+
 class SpectraController(PackageController):
 
     def all(self):
-        context = {'model': model, 'user': c.user}
-        data_dict = {
-            'query': 'name:esis_spectral_data.json'
-        }
-        query = logic.get_action('resource_search')(context, data_dict)
-        return self.stringify_json(query)
+        return self.stringify_json(infoCollection.find({},{'package_id':1}))
 
-    def add(self):
-        params = request.params
+    def addInfo(self):
+        info = request.params.get('info')
+        info = self.parse_json(info)
+        if 'package_id' not in info:
+            return self.stringify_json({'error':True, 'message':'No package_id provided'})
 
-        for item in params.multi.dicts:
-            if hasattr(item, '_items'):
-                if len(item._items) > 0:
-                    # for an update proly need to splice in here
-                    resource = self._create_resource(item._items)
-                    return self.stringify_json(resource)
+        cInfo = infoCollection.find_one({'package_id':info['package_id']})
+        if cInfo != None:
+            info['_id'] = cInfo['_id']
 
-        return self.stringify_json({'success':'false', 'error':'true'})
+        infoCollection.insert(info)
 
-    def _create_resource(self, tuple):
-        data_dict = {}
-        file = ''
-        filename = ''
+        return self.stringify_json({'success':True})
 
-        for key in tuple:
-            if key[0] == 'upload':
-                data = key[1]
-                file = data.file
-                filename = data.filename
-            else:
-                data_dict[key[0]] = key[1]
-        data_dict['url'] = filename
-        data_dict['mimetype'] = data_dict.get('mimetype')
-        data_dict['name'] = filename
-        data_dict['url_type'] = 'upload'
+    def addData(self):
+        data = self.parse_json(request.params.get('data'))
 
-        context = {'model': model, 'user': c.user}
-        resource = logic.get_action('resource_create')(context, data_dict)
+        if 'package_id' not in data:
+            return self.stringify_json({'error':True, 'message':'No package_id provided'})
 
-        upload = uploader.ResourceUpload(resource)
-        upload.filename = munge.munge_filename(filename)
+        # check package has item in info collection
+        if infoCollection.find_one({'package_id': data['package_id']}) == None:
+             return self.stringify_json({'error':True, 'message':'No package spectra found'})
 
-        file.seek(0)
-        upload.upload_file = file
-        upload.upload(resource.get('id'), uploader.get_max_resource_size())
+        # get current spectra for possible merge
+        currentData = dataCollection.find({'package_id': data['package_id']})
+        if currentData == None:
+            currentData = []
 
-        file.close()
+        for item in data['spectra']:
+            self._merge_and_insert(item, currentData)
 
-        return resource
+        return self.stringify_json({'success':True})
 
-    def update(self):
-        params = request.params
+    def _merge_and_insert(self, item, currentData):
+        for cItem in currentData:
+            if cItem['spectra_id'] == item['spectra_id']:
+                item['spectra'] = cItem['spectra']
+                item['_id'] = cItem['_id']
+                break
 
-        for item in params.multi.dicts:
-            if hasattr(item, '_items'):
-                if len(item._items) > 0:
-                    # for an update proly need to splice in here
-                    resource = self._update_resource(item._items)
-                    return self.stringify_json(resource)
+        dataCollection.insert(item)
 
-        return self.stringify_json({'success':'false', 'error':'true'})
-
-    def _update_resource(self, tuple):
-        data_dict = {}
-        file = ''
-        filename = ''
-
-        for key in tuple:
-            if key[0] == 'upload':
-                data = key[1]
-                file = data.file
-                filename = data.filename
-            else:
-                data_dict[key[0]] = key[1]
-
-
-        newDataset = self.parse_json(file.read())
-
-
-        # grab the old file
-        context = {'model': model, 'user': c.user}
-        resource = logic.get_action('resource_show')(context, {'id': data_dict['id']})
-        upload = uploader.ResourceUpload(resource)
-
-
-        oldfilename = upload.get_path(data_dict['id'])
-        oldfile = open(oldfilename, 'r')
-        oldDataset = self.parse_json(oldfile.read())
-
-        oldfile.close()
-
-        # now merge the file data
-        self._merge_dataset(oldDataset, newDataset)
-
-        # write new data
-        oldfile = open(oldfilename, 'w')
-        oldfile.write(json.dumps(newDataset))
-
-        file.close()
-        oldfile.close()
-
-        return resource
-
-    # loop through all of the old data fields,  old data fields will not have spectra, so reassociate
-    # based on spectra id
-    def _merge_dataset(self, old, new):
-        for oldData in old['data']:
-            for newData in new['data']:
-                if oldData['spectra_id'] == newData['spectra_id']:
-                    newData['spectra'] = oldData['spectra']
-                    break
-
-    # download all package resources
-    '''def download(self):
+    def deletePackage(self):
         id = request.params.get('id')
-        context = {'model': model, 'user': c.user}
-        pkg = logic.get_action('package_show')(context, {'id': id})
+        dataCollection.remove({'package_id':id})
+        infoCollection.remove({'package_id':id})
 
-        f = tempfile.TemporaryFile()
-        zf = zipfile.ZipFile(f, mode="w", compression=zipfile.ZIP_DEFLATED)
+    def deleteResource(self):
+        id = request.params.get('id')
+        dataCollection.remove({'resource_id':id})
 
-        for resource in pkg.get('resources'):
-            if resource.get('name') == 'esis_spectral_data.json':
-                continue
-
-            upload = uploader.ResourceUpload(resource)
-            zf.write(upload.get_path(resource.get('id')), arcname=resource.get('name'))
-
-        zf.close()
-        f.seek(0)
-
-        data = f.read()
-        f.close()'''
 
     def get(self):
         id = request.params.get('id')
         metadataOnly = request.params.get('metadataOnly')
 
-        context = {'model': model, 'user': c.user}
-        upload = uploader.ResourceUpload(logic.get_action('resource_show')(context, {'id': id}))
+        dataset = infoCollection.find_one({'package_id':id},{'_id':0})
 
-        dataset = ''
-        f = open(upload.get_path(id), 'r')
-        dataset = f.read()
-
+        cur = ''
+        dataset['data'] = []
         if metadataOnly == 'true':
-            # need to splice out the spectra
-            dataset = self.parse_json(dataset)
-            for item in dataset['data']:
-                item.pop("spectra", None)
-            dataset = self.stringify_json(dataset)
+            cur = dataCollection.find({'package_id':id}, {'spectra':0,'_id':0})
+        else:
+            cur = dataCollection.find({'package_id':id})
 
-        f.close()
+        for item in cur:
+            dataset['data'].append(item)
+
+        dataset = self.stringify_json(dataset)
 
         response.headers["Content-Type"] = "application/json"
         response.headers["Content-Length"] = "%s" % len(dataset)
 
         return dataset
 
-    def info(self):
-        id = request.params.get('id')
-        context = {'model': model, 'user': c.user}
-
-        resource = model.Resource.get(id)
-        pkg_id = resource.get_package_id()
-        pkg = logic.get_action('package_show')(context, {'id': pkg_id})
-
-        data = {
-            'pkg_id' : pkg_id,
-            'pkg_name' : pkg.get('name'),
-            'pkg_title' : pkg.get('title'),
-            'resource_id' : id,
-            'groups' : pkg.get('groups')
-        }
-
-        return self.stringify_json(data)
-
-    def createPackage(self):
+    def createPackageRedirect(self):
         response.status_int = 307
         response.headers["Location"] = "/editor/"
         return "Redirecting"
 
-    def editPackage(self, id):
+    def editPackageRedirect(self, id):
         response.status_int = 307
         response.headers["Location"] = "/editor/?id=%s" % id.encode('ascii','ignore')
         return "Redirecting"
