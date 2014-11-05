7c46f073e39b5afe1577b8612a9b3863e3ca99d0
Justin Merz <jrmerz@gmail.com>
2014-11-05 14:37:46 -0800

    updates

---
+++ spectra-importer/tests/upload.js
@@ -0,0 +1,33 @@
+var Browser = require('zombie');
+var assert  = require('assert');
+
+
+
+Browser.visit("http://localhost:3000/test.html", { 
+    debug: true, runScripts: true, waitFor : '10s', maxWait : '10s' ,
+        'function' : function(arg1, arg2, arg3, arg4) {
+            return arg1;
+        }
+    },
+             function (e, browser, status) {
+    
+
+    /*assert.ifError(e);
+    console.log(status);*/
+
+    browser.window.document.addEventListener('polymer-ready', function(){
+        console.log('Polymer is READY!!!')
+    });
+
+
+    browser.wait('15s', function(){
+        console.log('I am done waiting...');
+        setTimeout(function(){
+            console.log('Where we at?');
+            debugger;
+
+        }.bind(this), 5000);
+    });
+
+    
+});
