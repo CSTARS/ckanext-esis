82b012de46832a735c482869954e5438868d85b0
Justin Merz <jrmerz@gmail.com>
2015-04-27 10:37:30 -0400

    updating

--- spectra-importer/utils/metadata/create.js
+++ spectra-importer/utils/metadata/create.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
 var schema = fs.readFileSync(__dirname+'/../../../../ecosis-data-tool/app/schema.json');
-fs.writeFileSync(__dirname+'/../../core/schema.json', schema, 'utf-8');
+fs.writeFileSync(__dirname+'/../../app/schema.json', schema, 'utf-8');
 
 schema = JSON.parse(schema);
 var map = '';
 
 for( var key in schema ) {
     map += key+'\n';
-
+    
     for( var i = 0; i < schema[key].length; i++ ) {
         map += schema[key][i].name+'=\n';
     }
