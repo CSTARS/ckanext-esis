6a369bd68df5ce6c3e14190de989cc71aeb2b97f
Justin Merz <jrmerz@gmail.com>
2016-03-08 10:54:07 -0800

    updates for schema and input labels

--- spectra-importer/utils/metadata/schema/data/get.sh
+++ spectra-importer/utils/metadata/schema/data/get.sh
@@ -1,1 +1,1 @@
-wget -O test.csv "https://docs.google.com/spreadsheets/d/1jpKJ-P5X7q6UtPuKELxz4Eu8nJ3pAPf9NuYiygDzVss/export?format=csv&id=1jpKJ-P5X7q6UtPuKELxz4Eu8nJ3pAPf9NuYiygDzVss&gid=1840875846"
+wget -O tmp.csv "https://docs.google.com/spreadsheets/d/1jpKJ-P5X7q6UtPuKELxz4Eu8nJ3pAPf9NuYiygDzVss/export?format=csv&id=1jpKJ-P5X7q6UtPuKELxz4Eu8nJ3pAPf9NuYiygDzVss&gid=1840875846"
