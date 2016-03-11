622f3a6e85a13a114b6a232abed0a3e469c5ae80
Justin Merz <jrmerz@gmail.com>
2016-03-10 16:28:33 -0800

    update for lookup filters

--- ckanext/ecosis/datastore/mapreduce/lookup_map.js
+++ ckanext/ecosis/datastore/mapreduce/lookup_map.js
@@ -1,6 +1,6 @@
 function() {
     var pkg = this.value;
-    var keys = ['Keywords', 'Common Name', 'Latin Genus', 'Latin Species'];
+    var keys = ['Keywords', 'Common Name', 'Latin Genus', 'Latin Species', 'Theme'];
 
     for( var i = 0; i < keys.length; i++ ) {
         if( !pkg[keys[i]] ) continue;
@@ -9,5 +9,9 @@ function() {
         for( var j = 0; j < array.length; j++ ) {
             emit(keys[i]+'-'+array[j], {key: keys[i], value: array[j], count: 1})
         }
+    }
+
+    if( pkg.ecosis && pkg.ecosis.organization ) {
+        emit('ecosis.organization-'+pkg.ecosis.organization, {key: 'ecosis.organization', value: pkg.ecosis.organization, count: 1});
     }
 }
