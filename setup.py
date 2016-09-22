4420de4ede9b652d85c0ac7499734ef5b3121b4d
Justin Merz <jrmerz@gmail.com>
2016-09-22 10:33:27 -0700

    continued code cleanup

--- setup.py
+++ setup.py
@@ -10,22 +10,19 @@ setup(
         long_description="""\
         """,
         classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        keywords='',
+        keywords='Spectra',
         author='Justin Merz',
         author_email='jrmerz@ucdavis.edu',
         url='http://www.ucdavis.edu',
         license='',
         packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-        namespace_packages=['ckanext', 'ckanext.ecosis'],
+        namespace_packages=['ckanext'],
         include_package_data=True,
         zip_safe=False,
-        install_requires=[
-                # -*- Extra requirements: -*-
-        ],
+        install_requires=[],
         entry_points=\
         """
-        [ckan.plugins]
-        # Add plugins here, eg
-        ecosis=ckanext.ecosis.plugin:EcosisPlugin
+                [ckan.plugins]
+                ecosis=ckanext.ecosis.plugin:EcosisPlugin
         """,
 )
